af8caed3761136276c573295245f7605081417f8
John Doe <johndoe@example.com>
2023-12-01 01:29:25 -0600

    2023 Day 1 init

--- config.py
+++ config.py
@@ -3,9 +3,12 @@ import sys
 import os
 
 def main():    
-    DAY = sys.argv[1]
-    YEAR = sys.argv[2] if len(sys.argv) < 3 else "2023"
-    filepath = f"{YEAR}/Day{DAY : 03d}"
+    DAY = int(sys.argv[1])
+    if len(sys.argv) < 3:
+        YEAR = "2023"
+    else:
+      YEAR = sys.argv[2]   
+    filepath = f"{YEAR}/Day{DAY :02d}"
     
     print(f"~~~~~~~~~\n{filepath}\n~~~~~~~~")
     if not os.path.exists(YEAR):
@@ -15,7 +18,9 @@ def main():
 
     with open('cookie.txt','r') as file:
         SESSION = file.read() # string
-        os.system(f'curl https://adventofcode.com/2018/day/DAY/input --cookie "session={SESSION}" > {filepath}/input.txt')
+        cmd_str = f'curl https://adventofcode.com/{YEAR}/day/{DAY}/input --cookie "session={SESSION}" > {filepath}/input.txt'
+        print(cmd_str)
+        os.system(cmd_str)
 
 if __name__ == '__main__':
     main()
